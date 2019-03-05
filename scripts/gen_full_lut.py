7f005eabeaf61ed7ad0b7fe62c782da698de26fb
Linda <llinda13579@gmail.com>
2019-03-04 19:09:55 -0500

    uses a lutmask, fixed floating inputs

--- scripts/gen_full_lut.py
+++ scripts/gen_full_lut.py
@@ -2,27 +2,39 @@ import math
 
 def generate_full_lut(spice_file_name):
 
+	lut_mask = [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+	# lut_mask = [1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0]
+	# lut_mask = [1,1,0,0,1,1,0,0,1,1,0,0,1,1,0,0,1,1,0,0,1,1,0,0,1,1,0,0,1,1,0,0,1,1,0,0,1,1,0,0,1,1,0,0,1,1,0,0,1,1,0,0,1,1,0,0,1,1,0,0,1,1,0,0]
+	# lut_mask = [1,1,1,1,0,0,0,0,1,1,1,1,0,0,0,0,1,1,1,1,0,0,0,0,1,1,1,1,0,0,0,0,1,1,1,1,0,0,0,0,1,1,1,1,0,0,0,0,1,1,1,1,0,0,0,0,1,1,1,1,0,0,0,0]
+	# lut_mask = [1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0]
+	# lut_mask = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+	# lut_mask = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+
 	spice_file = open(spice_file_name, 'w')
 	sram_cell_count = 1;
 	lut_size = 6
 	lvl_curr = lut_size
+
+	lut_mask_index = 0;
 	# level 1
 	for i in range(1, 2**(lvl_curr-1)+1):
 		#spice_file.write("Xinv_sram_driver_1_"+str(i)+" n_sram_"+ str(sram_cell_count) + " n_inter_" + str(sram_cell_count) + " n_vdd gnd inv Wn=45n Wp=75.015'\n")
-		if (math.ceil(i%2)==0):
+		if (lut_mask[lut_mask_index] == 1):
 			spice_file.write("Xinv_sram_driver_1_"+str(i)+" vdd n_inter_" + str(sram_cell_count) + " n_vdd gnd inv Wn=45n Wp=75.015n\n")
 		else:
 			spice_file.write("Xinv_sram_driver_1_"+str(i)+" gnd n_inter_" + str(sram_cell_count) + " n_vdd gnd inv Wn=45n Wp=75.015n\n")	
+		lut_mask_index += 1
 		spice_file.write("Xwire_sram_driver_1_"+str(i)+" n_inter_"+ str(sram_cell_count) + " n_inter_2_" + str(sram_cell_count) + " wire Rw=wire_lut_sram_driver_res Cw=wire_lut_sram_driver_cap\n")		
 		spice_file.write("Xinv_sram_driver_2_"+str(i)+" n_inter_2_"+ str(sram_cell_count) + " n_inter_3_" + str(sram_cell_count) + " n_vdd gnd inv Wn=inv_lut_0sram_driver_2_nmos Wp=inv_lut_0sram_driver_2_pmos\n")
 		spice_file.write("Xwire_sram_driver_2_"+str(i)+" n_inter_3_"+ str(sram_cell_count) + " n_0_" + str(sram_cell_count) + " wire Rw=wire_lut_sram_driver_out_res Cw=wire_lut_sram_driver_out_cap\n")		
 		
 		#spice_file.write("Xinv_sram_driver_1_"+str(i)+" n_sram_"+ str(sram_cell_count+1) + " n_inter_" + str(sram_cell_count+1) + " n_vdd gnd inv Wn=45n Wp=75.015'\n")
 				#spice_file.write("Xinv_sram_driver_1_"+str(i)+" n_sram_"+ str(sram_cell_count) + " n_inter_" + str(sram_cell_count) + " n_vdd gnd inv Wn=45n Wp=75.015'\n")
-		if (math.ceil(i%2)==0):
-			spice_file.write("Xinv_sram_driver_1_2_"+str(i)+" gnd n_inter_" + str(sram_cell_count+1) + " n_vdd gnd inv Wn=45n Wp=75.015n\n")
-		else:
+		if (lut_mask[lut_mask_index] == 1):
 			spice_file.write("Xinv_sram_driver_1_2_"+str(i)+" vdd n_inter_" + str(sram_cell_count+1) + " n_vdd gnd inv Wn=45n Wp=75.015n\n")
+		else:
+			spice_file.write("Xinv_sram_driver_1_2_"+str(i)+" gnd n_inter_" + str(sram_cell_count+1) + " n_vdd gnd inv Wn=45n Wp=75.015n\n")
+		lut_mask_index += 1
 
 		spice_file.write("Xwire_sram_driver_1_2_"+str(i)+" n_inter_"+ str(sram_cell_count+1) + " n_inter_2_" + str(sram_cell_count+1) + " wire Rw=wire_lut_sram_driver_res Cw=wire_lut_sram_driver_cap\n")		
 		spice_file.write("Xinv_sram_driver_2_2_"+str(i)+" n_inter_2_"+ str(sram_cell_count+1) + " n_inter_3_" + str(sram_cell_count+1) + " n_vdd gnd inv Wn=inv_lut_0sram_driver_2_nmos Wp=inv_lut_0sram_driver_2_pmos\n")
@@ -49,7 +61,7 @@ def generate_full_lut(spice_file_name):
 	sram_cell_count = 1
 	for i in range(1, 2**(lvl_curr-1)+1):
 		spice_file.write("Xlut_buffer_"+str(i)+" n_3_"+ str(sram_cell_count) + " n_3_buff_" + str(sram_cell_count)+ " n_vdd gnd lut_buffer rest_lut_size=rest_lut_int_buffer_pmos inv_1_nmos=inv_lut_int_buffer_1_nmos inv_1_pmos=inv_lut_int_buffer_1_pmos Rw_int=wire_lut_int_buffer_res Cw_int=wire_lut_int_buffer_cap inv_2_nmos=inv_lut_int_buffer_2_nmos inv_2_pmos=inv_lut_int_buffer_2_pmos Rw_out=wire_lut_int_buffer_out_res Cw_out=wire_lut_int_buffer_out_cap \n")
-		sram_cell_count = sram_cell_count + 2
+		sram_cell_count = sram_cell_count + 1
 		
 	# level 4
 	lvl_curr=lvl_curr -1
@@ -76,7 +88,7 @@ def generate_full_lut(spice_file_name):
 	sram_cell_count = 1
 	for i in range(1, 2**(lvl_curr-1)+1):
 		spice_file.write("Xlut_buffer_"+str(i)+"_out n_6_"+ str(sram_cell_count) + " n_out n_vdd gnd " +  " lut_buffer rest_lut_size=rest_lut_out_buffer_pmos inv_1_nmos=inv_lut_out_buffer_1_nmos inv_1_pmos=inv_lut_out_buffer_1_pmos Rw_int=wire_lut_out_buffer_res Cw_int=wire_lut_out_buffer_cap inv_2_nmos=inv_lut_out_buffer_2_nmos inv_2_pmos=inv_lut_out_buffer_2_pmos Rw_out=0 Cw_out=0 \n")
-		sram_cell_count = sram_cell_count + 2	
+		sram_cell_count = sram_cell_count + 1	
 		
 		
 		
