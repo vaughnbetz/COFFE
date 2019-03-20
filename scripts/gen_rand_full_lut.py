88c84a1eaae9d10867da9de997a927e4d8ba8089
Linda <llinda13579@gmail.com>
2019-03-19 20:02:37 -0400

    Toggle one lut input at a time

--- scripts/gen_rand_full_lut.py
+++ scripts/gen_rand_full_lut.py
@@ -1,7 +1,16 @@
+# lut_mask = [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+# lut_mask = [1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0]
+# lut_mask = [1,1,0,0,1,1,0,0,1,1,0,0,1,1,0,0,1,1,0,0,1,1,0,0,1,1,0,0,1,1,0,0,1,1,0,0,1,1,0,0,1,1,0,0,1,1,0,0,1,1,0,0,1,1,0,0,1,1,0,0,1,1,0,0]
+# lut_mask = [1,1,1,1,0,0,0,0,1,1,1,1,0,0,0,0,1,1,1,1,0,0,0,0,1,1,1,1,0,0,0,0,1,1,1,1,0,0,0,0,1,1,1,1,0,0,0,0,1,1,1,1,0,0,0,0,1,1,1,1,0,0,0,0]
+# lut_mask = [1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0]
+# lut_mask = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+# lut_mask = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+# lut_mask = [0,1,1,0,1,0,0,1,1,0,0,1,0,1,1,0,1,0,0,1,0,1,1,0,0,1,1,0,1,0,0,1,1,0,0,1,0,1,1,0,0,1,1,0,1,0,0,1,0,1,1,0,1,0,0,1,1,0,0,1,0,1,1,0,] #XOR
+
 import math
 import random
 import sys
-
+import numpy as np
 
 def generate_headers(spice_file):
 	spice_file.write(".TITLE rand_lut\n" )
@@ -18,20 +27,23 @@ def generate_headers(spice_file):
 	spice_file.write(".MEASURE TRAN meas_avg_power PARAM = '-((meas_current1 + meas_current2)/4n)*supply_v'\n")
 	spice_file.write("\n")
 
-def generate_drivers(spice_file):
+def generate_drivers(spice_file, input_vector, input_driver):
 	drivers = ["a","b","c","d","e","f"]
-	sources = ["n_in_rise"
-			  ,"n_in_fall"
-			  ,"n_in_high"
-			  ,"n_in_low"]
+	sources_toggle = ["n_in_rise", "n_in_fall"]
+	sources_static = ["n_in_low", "n_in_high"]
 
+	driver_index = 0
 	for d in drivers:
-		s = sources[random.randint(0,3)]
+		if (input_driver == d):
+			s = sources_toggle[input_vector[driver_index]]
+		else:
+			s = sources_static[input_vector[driver_index]]
 		spice_file.write("Xcb_mux_on_" + d + " " + s + " n_1_1_d_" + d + " vsram vsram_n vdd gnd cb_mux_on\n")
 		spice_file.write("Xlocal_routing_wire_load_" + d + " n_1_1_d_" + d + " n_1_2_d_" + d + " vsram vsram_n vdd gnd vdd local_routing_wire_load\n")
 		spice_file.write("Xlut_" + d + "_driver_1 n_1_2_d_" + d + " n_" + d + " vsram vsram_n n_rsel n_2_1_d_" + d + " vdd gnd lut_" + d + "_driver\n")
 		spice_file.write("Xlut_" + d + "_driver_not_1 n_2_1_d_" + d + " n_" + d + "_b vdd gnd lut_" + d + "_driver_not\n")
 		spice_file.write("\n")
+		driver_index += 1
 
 
 def generate_footers(spice_file):
@@ -40,16 +52,8 @@ def generate_footers(spice_file):
 	spice_file.write(".END\n")
 	spice_file.write("\n")
 
-def generate_full_lut(spice_file, use_lutmask):
+def generate_full_lut(spice_file, lut_mask):
 
-	# lut_mask = [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-	# lut_mask = [1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0]
-	# lut_mask = [1,1,0,0,1,1,0,0,1,1,0,0,1,1,0,0,1,1,0,0,1,1,0,0,1,1,0,0,1,1,0,0,1,1,0,0,1,1,0,0,1,1,0,0,1,1,0,0,1,1,0,0,1,1,0,0,1,1,0,0,1,1,0,0]
-	# lut_mask = [1,1,1,1,0,0,0,0,1,1,1,1,0,0,0,0,1,1,1,1,0,0,0,0,1,1,1,1,0,0,0,0,1,1,1,1,0,0,0,0,1,1,1,1,0,0,0,0,1,1,1,1,0,0,0,0,1,1,1,1,0,0,0,0]
-	# lut_mask = [1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0]
-	# lut_mask = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-	# lut_mask = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-	lut_mask = [0,1,1,0,1,0,0,1,1,0,0,1,0,1,1,0,1,0,0,1,0,1,1,0,0,1,1,0,1,0,0,1,1,0,0,1,0,1,1,0,0,1,1,0,1,0,0,1,0,1,1,0,1,0,0,1,1,0,0,1,0,1,1,0,] #XOR
 
 	sram_cell_count = 1;
 	lut_size = 6
@@ -58,9 +62,7 @@ def generate_full_lut(spice_file, use_lutmask):
 	lut_mask_index = 0;
 	# level 1
 	for i in range(1, 2**(lvl_curr-1)+1):
-		#spice_file.write("Xinv_sram_driver_1_"+str(i)+" n_sram_"+ str(sram_cell_count) + " n_inter_" + str(sram_cell_count) + " n_vdd gnd inv Wn=45n Wp=75.015'\n")
-		if ((use_lutmask and lut_mask[lut_mask_index] == 1) or (not use_lutmask and random.randint(0,1) == 1)):
-		# if (random.randint(0,1) == 1):
+		if (lut_mask[lut_mask_index] == 1):
 			spice_file.write("Xinv_sram_driver_1_"+str(i)+" vdd n_inter_" + str(sram_cell_count) + " n_vdd gnd inv Wn=45n Wp=75.015n\n")
 		else:
 			spice_file.write("Xinv_sram_driver_1_"+str(i)+" gnd n_inter_" + str(sram_cell_count) + " n_vdd gnd inv Wn=45n Wp=75.015n\n")	
@@ -69,10 +71,7 @@ def generate_full_lut(spice_file, use_lutmask):
 		spice_file.write("Xinv_sram_driver_2_"+str(i)+" n_inter_2_"+ str(sram_cell_count) + " n_inter_3_" + str(sram_cell_count) + " n_vdd gnd inv Wn=inv_lut_0sram_driver_2_nmos Wp=inv_lut_0sram_driver_2_pmos\n")
 		spice_file.write("Xwire_sram_driver_2_"+str(i)+" n_inter_3_"+ str(sram_cell_count) + " n_0_" + str(sram_cell_count) + " wire Rw=wire_lut_sram_driver_out_res Cw=wire_lut_sram_driver_out_cap\n")		
 		
-		#spice_file.write("Xinv_sram_driver_1_"+str(i)+" n_sram_"+ str(sram_cell_count+1) + " n_inter_" + str(sram_cell_count+1) + " n_vdd gnd inv Wn=45n Wp=75.015'\n")
-				#spice_file.write("Xinv_sram_driver_1_"+str(i)+" n_sram_"+ str(sram_cell_count) + " n_inter_" + str(sram_cell_count) + " n_vdd gnd inv Wn=45n Wp=75.015'\n")
-		if ((use_lutmask and lut_mask[lut_mask_index] == 1) or (not use_lutmask and random.randint(0,1) == 1)):
-		# if (random.randint(0,1) == 1):
+		if (lut_mask[lut_mask_index] == 1):
 			spice_file.write("Xinv_sram_driver_1_2_"+str(i)+" vdd n_inter_" + str(sram_cell_count+1) + " n_vdd gnd inv Wn=45n Wp=75.015n\n")
 		else:
 			spice_file.write("Xinv_sram_driver_1_2_"+str(i)+" gnd n_inter_" + str(sram_cell_count+1) + " n_vdd gnd inv Wn=45n Wp=75.015n\n")
@@ -132,13 +131,23 @@ def generate_full_lut(spice_file, use_lutmask):
 		spice_file.write("Xlut_buffer_"+str(i)+"_out n_6_"+ str(sram_cell_count) + " n_out n_vdd gnd " +  " lut_buffer rest_lut_size=rest_lut_out_buffer_pmos inv_1_nmos=inv_lut_out_buffer_1_nmos inv_1_pmos=inv_lut_out_buffer_1_pmos Rw_int=wire_lut_out_buffer_res Cw_int=wire_lut_out_buffer_cap inv_2_nmos=inv_lut_out_buffer_2_nmos inv_2_pmos=inv_lut_out_buffer_2_pmos Rw_out=0 Cw_out=0 \n")
 		sram_cell_count = sram_cell_count + 1	
 		
+
+def generate_spice_file(spice_file, input_vector, driver, lut_mask):
+	generate_headers(spice_file)
+	generate_drivers(spice_file, input_vector, driver)
+	generate_full_lut(spice_file, lut_mask)
+	generate_footers(spice_file)
+
 		
-		
-use_lutmask = sys.argv[2] == "1"
-random.seed(sys.argv[1])
-spice_file = open("rand_lut.sp", 'w')
-generate_headers(spice_file)		
-generate_drivers(spice_file)		
-generate_full_lut(spice_file, use_lutmask)		
-generate_footers(spice_file)		
+np.random.seed(int(sys.argv[1]))
+
+lut_mask = np.random.randint(0,2,64)
+input_vector = np.random.randint(0,2,6)
+
+drivers = ["a","b","c","d","e","f","x"]
+
+for d in drivers:
+	spice_file = open("rand_lut_" + d + ".sp", 'w')
+	generate_spice_file(spice_file, input_vector, d, lut_mask)
+	spice_file.close()
 
--- scripts/run_seeds.sh
+++ scripts/run_seeds.sh
@@ -1,29 +1,49 @@
-
-
-echo "xor lut"
-
-for f in ../process_data_*00.l
-do
-echo $f
-cp $f ../process_data.l
-	for i in {1..250}
-	do
-	python ../../../COFFE/scripts/gen_rand_full_lut.py $i 1
-	hspice rand_lut.sp 2>&1 | grep meas_avg_power=
-	done
-done
-
-echo "random lut"
+echo "a" > a.txt
+echo "b" > b.txt
+echo "c" > c.txt
+echo "d" > d.txt
+echo "e" > e.txt
+echo "f" > f.txt
+echo "x" > x.txt
 
 for f in ../process_data_*00.l
 do
-echo $f
-cp $f ../process_data.l
-	for i in {1..250}
-	do
-	python ../../../COFFE/scripts/gen_rand_full_lut.py $i 0
-	hspice rand_lut.sp 2>&1 | grep meas_avg_power=
+	echo $f
+	cp $f ../process_data.l
+	echo $f >> a.txt
+	echo $f >> b.txt
+	echo $f >> c.txt
+	echo $f >> d.txt
+	echo $f >> e.txt
+	echo $f >> f.txt
+	echo $f >> x.txt
+	for i in {1..3}
+		do
+		python ../../../COFFE/scripts/gen_rand_full_lut.py $i
+		hspice rand_lut_a.sp 2>&1 | grep meas_avg_power= >> a.txt  &
+		hspice rand_lut_b.sp 2>&1 | grep meas_avg_power= >> b.txt  &
+		hspice rand_lut_c.sp 2>&1 | grep meas_avg_power= >> c.txt  &
+		hspice rand_lut_d.sp 2>&1 | grep meas_avg_power= >> d.txt  &
+		hspice rand_lut_e.sp 2>&1 | grep meas_avg_power= >> e.txt  &
+		hspice rand_lut_f.sp 2>&1 | grep meas_avg_power= >> f.txt  &
+		hspice rand_lut_x.sp 2>&1 | grep meas_avg_power= >> x.txt  &
+		wait
 	done
 done
-
+sed 's/ meas_avg_power= *//' a.txt > a_power.txt &
+sed 's/ meas_avg_power= *//' b.txt > b_power.txt &
+sed 's/ meas_avg_power= *//' c.txt > c_power.txt &
+sed 's/ meas_avg_power= *//' d.txt > d_power.txt &
+sed 's/ meas_avg_power= *//' e.txt > e_power.txt &
+sed 's/ meas_avg_power= *//' f.txt > f_power.txt &
+sed 's/ meas_avg_power= *//' x.txt > x_power.txt &
+wait
+rm a.txt &
+rm b.txt &
+rm c.txt &
+rm d.txt &
+rm e.txt &
+rm f.txt &
+rm x.txt &
+wait
 echo "done"
